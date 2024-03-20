739ff5df0f21496f9e9a466703bea8dd16dac95e
Samiyah Hoque <samiyahhoque22@gmail.com>
2024-03-20 12:06:58 -0400

    CLI + peer discovery

--- server/main.go
+++ server/main.go
@@ -22,252 +22,228 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"log"
+	"math/rand"
+	"sync"
 
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 	"github.com/multiformats/go-multiaddr"
 	"google.golang.org/protobuf/proto"
 
 	pb "orcanet/market"
 )
 
-/*
-
 var (
-	port = flag.Int("port", 50051, "The server port")
+	clientMode           = flag.Bool("client", false, "run this program in client mode")
+	startBootstrapNodeAt = flag.String("startBootstrapNodeAt", "", "start a bootstrap node")
+	bootstrap            = flag.String("bootstrap", "", "multiaddresses to bootstrap to")
+	addr                 = flag.String("addr", "", "multiaddresses to listen to")
 )
 
-// map file hashes to supplied files + prices
-var files = make(map[string][]*pb.RegisterFileRequest)
-
-// print the current holders map
-func printHoldersMap() {
-	for hash, holders := range files {
-		fmt.Printf("\nFile Hash: %s\n", hash)
-
-		for _, holder := range holders {
-			user := holder.GetUser()
-			fmt.Printf("Username: %s, Price: %d\n", user.GetName(), user.GetPrice())
-		}
-
-	}
-}
-
-type server struct {
-	pb.UnimplementedMarketServer
-}
-
-*/
-
-// DefaultBootstrapPeers is a set of public DHT bootstrap peers provided by libp2p.
-// obtained here https://github.com/libp2p/go-libp2p-kad-dht/blob/master/dht_bootstrap.go
-var DefaultBootstrapPeers = []string{
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-    "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
-    "/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ", 
-}
-
-
-// GetDefaultBootstrapPeerAddrInfos parses the bootstrap peers' multiaddresses
-// into peer.AddrInfo objects for libp2p networking.
-func GetDefaultBootstrapPeerAddrInfos() ([]peer.AddrInfo, error) {
-    var peers []peer.AddrInfo
-    for _, addrStr := range DefaultBootstrapPeers {
-
-		// Convert the string to a multiaddr.Multiaddr object
-        addr, err := multiaddr.NewMultiaddr(addrStr)
-        if err != nil {
-            return nil, err
-        }
-
-		//construct a peer.AddrInfo object from the Multiaddr above 
-        pi, err := peer.AddrInfoFromP2pAddr(addr)
-        if err != nil {
-            return nil, err
-        }
-
-		//append peers
-        peers = append(peers, *pi)
-    }
-
-	//returns list of AddrInfo objects 
-    return peers, nil
-}
-
-
-//function to iterate over addr info objects and connect 
-func connectToBootstrapPeers(ctx context.Context, h host.Host, bootstrapPeers []peer.AddrInfo) {
-    for _, peerInfo := range bootstrapPeers {
-		//attempt to connect
-        if err := h.Connect(ctx, peerInfo); err != nil {
-            log.Printf("Failed to connect to bootstrap peer %v: %s", peerInfo, err)
-        } else {
-            fmt.Printf("Connected to bootstrap peer: %s\n", peerInfo.ID)
-        }
-    }
-}
-
-
-
-// each key needs to validated before being put in the DHT. For now we accept all hashes
-// https://github.com/libp2p/specs/tree/master/kad-dht#entry-validation
-
-// create custom validator function
 type CustomValidator struct{}
 
-//validator for key
-func (cv CustomValidator) Select( string,  [][]byte) (int, error) {
-	
-	//accept all for now
-    return 0, nil 
+func (cv CustomValidator) Select(string, [][]byte) (int, error) {
+	return 0, nil
 }
 
-// validates whether a value is acceptable or not
 func (cv CustomValidator) Validate(key string, value []byte) error {
-    
-	//accept all for now 
-    return nil
+	return nil
 }
-
 
 func main() {
-    ctx := context.Background()
+	ctx := context.Background()
+	flag.Parse()
 
-    // Initialize a new libp2p Host
-    host, err := libp2p.New()
-    if err != nil {
-        log.Fatalf("Failed to create host: %s", err)
-    }
+	// Convert the strings (from flags) to multiaddresses
+	listenAddr, _ := multiaddr.NewMultiaddr(*addr)
+	// startBootstrapAddr, _ := multiaddr.NewMultiaddr(*startBootstrapNodeAt)
 
-    // Initialize the DHT with the host
-    kademliaDHT, err := dht.New(ctx, host)
-    if err != nil {
-        log.Fatalf("Failed to create DHT: %s", err)
-    }
+	var bootstrapPeers []multiaddr.Multiaddr
 
-    // Bootstrap the DHT
-    if err := kademliaDHT.Bootstrap(ctx); err != nil {
-        log.Fatalf("Failed to bootstrap DHT: %s", err)
-    }
+	// Check if a bootstrap address is provided.
+	if len(*bootstrap) > 0 {
+		// Try to create a multiaddr from the provided bootstrap flag.
+		bootstrapAddr, err := multiaddr.NewMultiaddr(*bootstrap)
+		if err != nil {
+			// Handle the error properly (e.g., log it or exit).
+			fmt.Errorf("Invalid bootstrap address: %s", err)
+		}
+		// Use the provided bootstrap address.
+		bootstrapPeers = append(bootstrapPeers, bootstrapAddr)
+	} else {
+		// Use default bootstrap peers if no address is provided.
+		bootstrapPeers = dht.DefaultBootstrapPeers
+	}
+
+	// Initialize a new libp2p Host
+	host, err := libp2p.New(libp2p.ListenAddrs(listenAddr))
+	if err != nil {
+		fmt.Errorf("Failed to create host: %s", err)
+	}
+	fmt.Println("Host created with ID ", host.ID())
+
+	// Initialize the DHT
+	kademliaDHT, err := dht.New(ctx, host)
+	if err != nil {
+		fmt.Errorf("Failed to create DHT: %s", err)
+	}
+	fmt.Println("DHT created")
+
+	// Bootstrap the DHT
+	if err := kademliaDHT.Bootstrap(ctx); err != nil {
+		fmt.Errorf("Failed to bootstrap DHT: %s", err)
+	}
 
 	// Connect the validator
 	kademliaDHT.Validator = &CustomValidator{}
 
-    // Retrieve and connect to default bootstrap peers (hardcoded list above)
-    bootstrapPeers, err := GetDefaultBootstrapPeerAddrInfos()
-    if err != nil {
-        log.Fatalf("Failed to get default bootstrap peers: %s", err)
-    }
-    connectToBootstrapPeers(ctx, host, bootstrapPeers)
+	connectToBootstrapPeers(ctx, host, bootstrapPeers)
 
-	//print our host id
-    fmt.Println("Host created. Host ID:", host.ID())
-	
-	// test example
-    req := &pb.RegisterFileRequest{
-        User: &pb.User{
-            Id:    "user1",
-            Name:  "Jackie",
-            Ip:    "127.0.0.1",
-            Port:  4001,
-            Price: 100,
-        },
-        FileHash: "testhash",
-    }
-    if err := registerFileToDHT(context.Background(), kademliaDHT, req); err != nil {
-        log.Fatal(err)
-    }
+	// Prompt for username in terminal
+	var username string
+	fmt.Print("Enter username: ")
+	fmt.Scanln(&username)
 
-    // Check for holders
-    checkReq := &pb.CheckHoldersRequest{FileHash: "testhash"}
-    holdersResp, err := checkHoldersFromDHT(context.Background(), kademliaDHT, checkReq)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Holders:", holdersResp.Holders)
+	// Generate a random ID for new user
+	userID := fmt.Sprintf("user%d", rand.Intn(10000))
 
-}
-
-// register that the a user holds a file, then add the user to the list of file holders
-func registerFileToDHT(ctx context.Context, dht *dht.IpfsDHT, req *pb.RegisterFileRequest) error {
-	//serialize the User object to byte slice for storage
-    data, err := proto.Marshal(req.User)
-    if err != nil {
-        errMsg := fmt.Sprintf("Error marshaling user data for file hash %s: %v", req.FileHash, err)
-        log.Println(errMsg)
-        return fmt.Errorf(errMsg) 
-    }
-
-	
-    // Use the file hash as the key to store serialized user data in the DHT
-	// for now use file hash provided by user in Req object
-    key := fmt.Sprintf("/market/file/%s", req.FileHash)
-    if err := dht.PutValue(ctx, key, data); err != nil {
-        errMsg := fmt.Sprintf("Error putting value in the DHT for file hash %s: %v", req.FileHash, err)
-        log.Println(errMsg)
-        return fmt.Errorf(errMsg) 
-    }
-
-    fmt.Printf("Successfully registered file with hash %s\n", req.FileHash)
-    return nil
-}
-
-
-
-// CheckHolders returns a list of user names holding a file with a hash
-func checkHoldersFromDHT(ctx context.Context, dht *dht.IpfsDHT, req *pb.CheckHoldersRequest) (*pb.HoldersResponse, error) {
-    key := fmt.Sprintf("/market/file/%s", req.FileHash)
-    data, err := dht.GetValue(ctx, key)
-    if err != nil {
-        log.Printf("Failed to get value from the DHT: %v", err)
-        return nil, err
-    }
-
-    // Deserialize the data back into a User struct
-    var user pb.User
-    if err := proto.Unmarshal(data, &user); err != nil {
-        log.Printf("Failed to unmarshal user data: %v", err)
-        return nil, err
-    }
-
-    // Wrap the User in a HoldersResponse and return
-    return &pb.HoldersResponse{Holders: []*pb.User{&user}}, nil
-}
-
-
-
-/*
-
-// register that the a user holds a file, then add the user to the list of file holders
-func (s *server) RegisterFile(ctx context.Context, in *pb.RegisterFileRequest) (*emptypb.Empty, error) {
-	hash := in.GetFileHash()
-
-	files[hash] = append(files[hash], in)
-	fmt.Printf("Num of registered files: %d\n", len(files[hash]))
-	return &emptypb.Empty{}, nil
-}
-
-// CheckHolders returns a list of user names holding a file with a hash
-func (s *server) CheckHolders(ctx context.Context, in *pb.CheckHoldersRequest) (*pb.HoldersResponse, error) {
-	hash := in.GetFileHash()
-
-	holders := files[hash]
-
-	users := make([]*pb.User, len(holders))
-	for i, holder := range holders {
-		users[i] = holder.GetUser()
+	fmt.Print("Enter a price for supplying files: ")
+	var price int64
+	_, err = fmt.Scanln(&price)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
 	}
 
-	printHoldersMap()
+	// Create a User struct with the provided username and generated ID
+	user := &pb.User{
+		Id:    userID,
+		Name:  username,
+		Ip:    "localhost",
+		Port:  416320,
+		Price: price,
+	}
 
-	return &pb.HoldersResponse{Holders: users}, nil
+	for {
+		go peerDiscovery(ctx, host, kademliaDHT)
+
+		fmt.Println("---------------------------------")
+		fmt.Println("1. Register a file")
+		fmt.Println("2. Check holders for a file")
+		fmt.Println("3. Exit")
+		fmt.Print("Option: ")
+		var choice int
+		_, err := fmt.Scanln(&choice)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			continue
+		}
+
+		if choice == 3 {
+			return
+		}
+
+		fmt.Print("Enter a file hash: ")
+		var fileHash string
+		_, err = fmt.Scanln(&fileHash)
+		if err != nil {
+			fmt.Errorf("Error: ", err)
+			continue
+		}
+
+		switch choice {
+		case 1:
+			req := &pb.RegisterFileRequest{User: user, FileHash: fileHash}
+			registerFile(ctx, kademliaDHT, req)
+		case 2:
+			checkReq := &pb.CheckHoldersRequest{FileHash: fileHash}
+			holdersResp, _ := checkHolders(ctx, kademliaDHT, checkReq)
+			fmt.Println("Holders:", holdersResp.Holders)
+		case 3:
+			return
+		default:
+			fmt.Println("Unknown option: ", choice)
+		}
+
+		fmt.Println()
+	}
 }
 
+func connectToBootstrapPeers(ctx context.Context, host host.Host, bootstrapPeers []multiaddr.Multiaddr) {
+	var wg sync.WaitGroup
+	for _, peerAddr := range bootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerinfo); err != nil {
+				fmt.Errorf("Failed to connect to bootstrap peer %v: %s", peerinfo, err)
+			} else {
+				fmt.Printf("Connected to bootstrap peer: %s\n", peerinfo.ID)
+			}
+		}()
+	}
+	wg.Wait()
+}
 
-*/
+func peerDiscovery(ctx context.Context, host host.Host, dht *dht.IpfsDHT) {
+	routingDiscovery := drouting.NewRoutingDiscovery(dht)
+	dutil.Advertise(ctx, routingDiscovery, "market")
+	peerChan, _ := routingDiscovery.FindPeers(ctx, "market")
+
+	for peer := range peerChan {
+		if peer.ID == host.ID() {
+			continue
+		}
+		fmt.Println("Found peer:", peer)
+	}
+}
+
+// register that the a user holds a file, then add the user to the list of file holders
+func registerFile(ctx context.Context, dht *dht.IpfsDHT, req *pb.RegisterFileRequest) error {
+	//serialize the User object to byte slice for storage
+	data, err := proto.Marshal(req.User)
+	if err != nil {
+		errMsg := fmt.Sprintf("Error marshaling user data for file hash %s: %v", req.FileHash, err)
+		fmt.Println(errMsg)
+		return fmt.Errorf(errMsg)
+	}
+
+	// Use the file hash as the key to store serialized user data in the DHT
+	// for now use file hash provided by user in Req object
+	key := fmt.Sprintf("/market/file/%s", req.FileHash)
+	if err := dht.PutValue(ctx, key, data); err != nil {
+		errMsg := fmt.Sprintf("Error putting value in the DHT for file hash %s: %v", req.FileHash, err)
+		fmt.Println(errMsg)
+		return fmt.Errorf(errMsg)
+	}
+
+	fmt.Printf("Successfully registered file with hash %s\n", req.FileHash)
+	return nil
+}
+
+// CheckHolders returns a list of user names holding a file with a hash
+func checkHolders(ctx context.Context, dht *dht.IpfsDHT, req *pb.CheckHoldersRequest) (*pb.HoldersResponse, error) {
+	key := fmt.Sprintf("/market/file/%s", req.FileHash)
+	data, err := dht.GetValue(ctx, key)
+	if err != nil {
+		fmt.Printf("Failed to get value from the DHT: %v", err)
+		return nil, err
+	}
+
+	// Deserialize the data back into a User struct
+	var user pb.User
+	if err := proto.Unmarshal(data, &user); err != nil {
+		fmt.Printf("Failed to unmarshal user data: %v", err)
+		return nil, err
+	}
+
+	// Wrap the User in a HoldersResponse and return
+	return &pb.HoldersResponse{Holders: []*pb.User{&user}}, nil
+}
